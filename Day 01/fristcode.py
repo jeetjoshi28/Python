7fbd34a42ddffa696130d796c7ecac4b2656203c
joshijeetk <jeet@anhasweb.com>
2026-02-03 23:55:37 +0530

    python basic

---
+++ Day 01/fristcode.py
@@ -0,0 +1,19 @@
+# In the python to print something use this function print() using this function we can print anything on the screen.
+
+print('Hello, My name is Jeet Joshi!!')
+
+print("I am learning Python Programming.")
+
+# In the python both are allow ' ' as well ""
+
+# Code excerice
+
+# print this line same in the console.
+
+# Frist Program - Python print function
+# It is declared like this:
+# print ('What to print')
+
+print("Frist Program - Python print function")
+print("It is declared like this:")
+print("print ('What to print')")
---
+++ Day 01/user_input.py
@@ -0,0 +1,5 @@
+# in the python to get the input from the user we use the input() function
+
+name  = input("Enter your name: ")
+print("What is your name? " + name)
+print("Hey" " " + name + ", how are you?")
